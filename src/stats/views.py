3728212466f1631b76d7e369eabe9d7a8ef51191
Abdellah <skoundriabdellah@gmail.com>
2020-08-31 21:01:49 +0200

    add loop dynamic data

--- src/stats/views.py
+++ src/stats/views.py
@@ -7,5 +7,5 @@ def dashboard(request, days_range=60, currencies="CAD"):
 
     days, rates = api.get_rates(curriences=currencies.split(","), days=days_range)
 
-    return render(request, "stats/index.html", context={"data": rates["CHF"],
+    return render(request, "stats/index.html", context={"data": rates,
                                                         "days_labels": days})
